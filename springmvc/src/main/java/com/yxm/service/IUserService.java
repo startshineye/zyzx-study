7307437f5fb1feff8cd339fecc203b4abdc6939c
startshineye <13001955858@163.com>
2018-04-18 20:21:29 +0800

    2018/4/18/1

---
+++ springmvc/src/main/java/com/yxm/service/IUserService.java
@@ -0,0 +1,5 @@
+package com.yxm.service;
+
+public interface IUserService {
+    public String getName(String name);
+}
---
+++ springmvc/src/main/java/com/yxm/service/Impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.yxm.service.Impl;
+
+import org.springframework.stereotype.Service;
+
+import com.yxm.service.IUserService;
+
+@Service("userService")
+public class UserServiceImpl implements IUserService {
+
+	public String getName(String name) {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+}
