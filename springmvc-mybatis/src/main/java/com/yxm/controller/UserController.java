a425ccb34218973d8cf09081124ed6f617e2e4c5
startshineye <13001955858@163.com>
2018-04-22 18:22:27 +0800

    2018/4/22/2

--- springmvc-mybatis/src/main/java/com/yxm/controller/UserController.java
+++ springmvc-mybatis/src/main/java/com/yxm/controller/UserController.java
@@ -3,16 +3,18 @@ package com.yxm.controller;
 import javax.annotation.Resource;
 
 import com.yxm.domain.Student;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yxm.service.IUserService;
 
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
-     
+     Logger logger = Logger.getLogger(UserController.class);
 	 @Resource
 	 private IUserService userService;
 	 
@@ -20,12 +22,15 @@ public class UserController {
 	 @RequestMapping("/getname")
 	 @ResponseBody
 	 public String getName(){
-		 return userService.getName("aaa");
+		logger.info("==info== UserController 中的 getName方法执行");
+		logger.error("==error== UserController 中的 getName方法执行");
+	 	return userService.getName("aaa");
 	 }
 
 	@RequestMapping("/getNameById")
 	@ResponseBody
 	 public Student getStudent(){
+
        return userService.getStudentById(1);
 	 }
 	@RequestMapping("/delete")
